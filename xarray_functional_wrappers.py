20bf86e98dedc741d1d1e51ebe60716ee18915be
Arthur Adams <arthur.daniel.adams@gmail.com>
2024-11-08 11:30:23 -0500

    Incremental addition of notes and ideas for future work: the existing Python version of the 2-stream RT scheme, implementation examples for xarray's apply_ufunc, and pulling over the idea for a function-specific input file format from the Apollo development.

--- xarray_functional_wrappers.py
+++ xarray_functional_wrappers.py
@@ -3,6 +3,7 @@ from functools import partial, reduce
 from inspect import signature
 from typing import Protocol
 
+import numpy as np
 import xarray as xr
 
 # NOTE to self: assess whether we can develop a version of these that uses xr.apply_ufunc;
@@ -108,3 +109,105 @@ def map_to_xarray(
 #       nature of the variables (e.g. do they represent pressure, temperature, wavelength, etc.),
 #       and potentially the shapes of the inputs vs. outputs. Not specific shapes, just for example
 #       whether the function preserves the shape of the input.
+
+
+def wrap_ufunc(dataset: xr.Dataset, variable_name_mapping: dict[str, str]) -> Callable:
+    # map_function_arguments_to_dataset_variables -> function version that takes dataset as a single argument
+
+    """
+    def squared_error(x, y):
+        return (x - y) ** 2
+
+    result = xr.apply_ufunc(squared_error, ds.air, 1)
+
+    def wrapper(x, y):
+        print(f"received x of type {type(x)}, shape {x.shape}")
+        print(f"received y of type {type(y)}")
+        return squared_error(x, y)
+
+    result = xr.apply_ufunc(wrapper, ds.air, 1, keep_attrs=True)
+
+    ds2 = ds.copy()
+    ds2["air2"] = ds2.air.isel(lon=0) ** 2
+
+    result = xr.apply_ufunc(wrapper, ds2, 1)
+    result = xr.apply_ufunc(squared_error, ds2, 1)
+
+    result = xr.apply_ufunc(squared_error, ds.air, kwargs={"y": 1})
+
+    result = xr.apply_ufunc(
+        # function to apply
+        np.mean,
+        # object with data to pass to function
+        ds,
+        # keyword arguments to pass to np.mean
+        kwargs={"axis": 0},
+    )
+
+    result = xr.apply_ufunc(
+        np.mean,
+        ds, # (time, lat, lon)
+        # specify core dimensions as a list of lists
+        # here 'time' is the core dimension on `ds`
+        input_core_dims=[
+            ["time"],  # core dimension for ds
+        ],
+        kwargs={"axis": 0},
+    )
+
+    # With apply_ufunc, core dimensions are recognized by name, and then moved to the last dimension of any input arguments before applying the given function. This means that for functions that accept an axis argument, you usually need to set axis=-1.
+    def wrapper(array, **kwargs):
+        print(f"received {type(array)} shape: {array.shape}, kwargs: {kwargs}")
+        result = np.mean(array, **kwargs)
+        print(f"result.shape: {result.shape}")
+        return result
+
+
+    result = xr.apply_ufunc(
+        wrapper,
+        ds,
+        input_core_dims=[["time"]],
+        kwargs={"axis": -1},
+    )
+
+    interp_result = np.interp(newlat, air.lat.data, air.data)
+
+    ### EITHER:
+
+    interp_result = xr.apply_ufunc(
+        np.interp,  # function to apply
+        newlat,  # 1st input to np.interp
+        air.lat,  # 2nd input to np.interp
+        air,  # 3rd input to np.interp
+        input_core_dims=[["lat_interp"], ["lat"], ["lat"]],  # one entry per function input, 3 in total!
+        output_core_dims=[["lat_interp"]],
+    )
+
+    ### OR:
+
+    interp_result = xr.apply_ufunc(
+        np.interp,  # first the function
+        newlat,
+        air.lat,
+        air,
+        input_core_dims=[["lat"], ["lat"], ["lat"]],
+        output_core_dims=[["lat"]],
+        exclude_dims={"lat"},
+    )
+
+    def minmax(array):
+        return array.min(axis=-1), array.max(axis=-1)
+
+    minmax_result = minda, maxda = xr.apply_ufunc(
+        minmax,
+        air2d,
+        input_core_dims=[["lat"]],
+        output_core_dims=[[], []],
+    )
+
+    """
+
+    def wrap_apply_ufunc(function: np.ufunc):
+        return
+
+    return
